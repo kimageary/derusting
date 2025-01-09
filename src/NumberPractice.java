bd8a7de655a38fadbc9abca6af9b0fc0fdd415c3
Kimberly Mageary <kimberlymageary@gmail.com>
2025-01-08 16:07:56 -0800

    Solved number practice

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,18 +1,31 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float newFloat = -4.545f;
+    System.out.println(newFloat);
 
     // Create an int with a positive value and assign it to a variable
+    int newInt = 36;
+    System.out.println(newInt);
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    float remainder = newInt % 3;
+    System.out.println(remainder);
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+      if(newInt % 2 == 0){
+        System.out.println("This number is even");
+      }else{
+        System.out.println("This number is odd!");
+      }
 
     // Divide the number by another number using integer division
 
+    int division = newInt / 4;
+    System.out.println(division);
     /*
      * Reminder!
      * 
